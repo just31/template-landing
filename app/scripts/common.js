0cd39a056bb4788cebfaf134038936313d93437d
Evgeny Moiseev <just8@mail.ru>
2015-09-14 15:28:14 +0300

    Исходники проекта. Оптимизация скрипта слайдера, более плавное проявление слайдов. Также слайды теперь появляются без сильного смещения вверх.

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -3,15 +3,34 @@ $(function () {
 	'use strict';
 
     // Слайдер, переключение между картинками 'os-x' и 'windows'.
-	$('.header__button a').click(function(e){
-	    e.preventDefault();
-		$('.header__button .header__button_active').removeClass('header__button_active');
-		$(this).addClass('header__button_active');
-		var tab = $(this).attr('href');
-		$('.header__choose-devices__tab').not(tab).css({'display':'none'});
-		$(tab).fadeIn(400);
 
-        $('#tab2__img').css({'margin-top' : '0', 'margin-left' : '0'});
+	$(document).ready(function(){
+	    $('.header__button a').click(function(e){
+		    e.preventDefault();
+			$('.header__button .header__button_active').removeClass('header__button_active');
+			$(this).addClass('header__button_active');
+			var tab = $(this).attr('href');
+            $('.header__choose-devices__tab').not(tab).animate({'z-index' : 100 , 'opacity' : 1}, 1000);
+			$(tab).animate({'z-index' : 10 , 'opacity' : .01}, 1000);
+
+            $('#tab2__img').css({'margin-top' : '-180px', 'margin-left' : '42px'});
+		});
+
 	});
+
+    /*
+    $(document).ready(function(){
+	    $('.header__button a').click(function(e){
+		    e.preventDefault();
+			$('.header__button .header__button_active').removeClass('header__button_active');
+			$(this).addClass('header__button_active');
+			var tab = $(this).attr('href');
+            $('.header__choose-devices__tab').not(tab).css({'display' : 'none'});
+			$(tab).fadeIn(1000);
+
+            $('#tab2__img').css({'margin-top' : '0', 'margin-left' : '0'});
+		});
+	});
+    */
 
 });
